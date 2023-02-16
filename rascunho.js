5b4fe2b9273848946fcdeee73d2664101d933af6
Guilherme A <guilhermealc01@gmail.com>
2023-02-15 22:43:55 -0300

    enviando arquivos do curso de algoritmo parte 2

---
+++ rascunho.js
@@ -0,0 +1,4 @@
+const branco = ["camel", "hollywood", "malboro", "derb", "kent"]
+const palha = ["porto faria", "mandelle", "coyote"]
+const todos = []
+console.log(todos.concat(branco.slice(2), palha.slice(0)))
